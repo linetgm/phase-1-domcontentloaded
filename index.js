96c5a5c07897b4b0eaa2333e0b22484caa011eca
Linet <muthiigathoni@gmail.com>
2023-06-29 10:10:30 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const change = document.getElementById('text');
+    change.innerHTML = "This is really cool!";
+})
